321804c3b0b7eace3d7a2f0021f1d56aa358d8e5
Mavaddat Javid <5055400+mavaddat@users.noreply.github.com>
2023-09-12 19:56:53 -0400

    reporting on CSV, TSV, and XML

--- UowState.cs
+++ UowState.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Diagnostics;
 using System.Reflection;
+using System.Xml;
 
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("BaXmlSplitter.Tests")]
 
